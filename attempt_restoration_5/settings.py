e26acbc5e5b32821e6c573f5f7a323a4e359f955
good-margaret <tishchenko-margarita@mail.ru>
2025-05-28 02:46:19 +0300

    postgres added

--- attempt_restoration_5/settings.py
+++ attempt_restoration_5/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = "attempt_restoration_5.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photo_restorer',
+        'USER': 'postgres',
+        'PASSWORD': 'bad00marigo',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
